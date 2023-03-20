b165983aed489296dfbcb988b8ea4a2ab01c8cac
Wendina <wendina.teng@gmail.com>
2023-03-20 23:43:00 +0000

    Added a handler for additional routes. Used res.json to send a JSON string represenation of the entire urlDatabase object that was passed in. Added comments in the file for further clarity.

--- express_server.js
+++ express_server.js
@@ -7,8 +7,15 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+// Handler code on the root path '/'
 app.get("/", (req, res) => {
   res.send("Hello!");
+});
+
+// Handler code on additional endpoints.
+// res.json sends a JSON response => expect to see a JSON string representing the entire urlDatabase object 
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
 });
 
 app.listen(PORT, () => {
