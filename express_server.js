e36168f82a9eb689bd0df3e1a6c53746034024a5
Wendina <wendina.teng@gmail.com>
2023-03-21 00:07:04 +0000

    Added a handler code to send HTML, which would be rendered in the client browser. Added a comment for clarity.

--- express_server.js
+++ express_server.js
@@ -18,6 +18,12 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+// Handler code on sending HTML, which would be rendered in the client browser
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
